182f1be5c8a943cb91c4d85121a1ad3106bbdebe
LYL <38547725+GentleTK@users.noreply.github.com>
2019-06-22 08:44:37 +0000

    Delete derby.sql

--- derby.sql
+++
@@ -1,8 +0,0 @@
-create table mytable(    
-  word  varchar(30),  
-  chinese   varchar(30)  
-);  
-insert into mytable values('apple','Æ»¹û');  
-insert into mytable values('big','´óµÄ');  
-insert into mytable values('cat','Ã¨');  
-select * from mytable;  
